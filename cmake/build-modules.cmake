e4c901ab8e8ed8c2701fe8733ded62cb6878e66c
Xavier Liu <i@xavierliu.io>
2024-07-25 23:43:29 +0800

    Changed indentation from tabs to spaces

--- cmake/build-modules.cmake
+++ cmake/build-modules.cmake
@@ -2,43 +2,43 @@ include("cmake/utils.cmake")
 
 macro(m_define_module _v_module_name)
 
-	set(v_module_name "${_v_module_name}")
-	m_configure_project_env(${_v_module_name})
+    set(v_module_name "${_v_module_name}")
+    m_configure_project_env(${_v_module_name})
 
-	project(${v_project} LANGUAGES CXX)
+    project(${v_project} LANGUAGES CXX)
 
-	add_library(${v_project} STATIC)
-	add_library(${v_project_alias} ALIAS ${v_project})
+    add_library(${v_project} STATIC)
+    add_library(${v_project_alias} ALIAS ${v_project})
 
-	m_set_target_flags(${v_project})
+    m_set_target_flags(${v_project})
 endmacro()
 
 macro(m_define_test _v_test_name)
 
-	set(v_test_name "${_v_test_name}")
-	m_configure_project_env(${v_test_name})
+    set(v_test_name "${_v_test_name}")
+    m_configure_project_env(${v_test_name})
 
-	project(${v_project} LANGUAGES CXX)
+    project(${v_project} LANGUAGES CXX)
 
-	add_executable(${v_project})
-	add_executable(${v_project_alias} ALIAS ${v_project})
+    add_executable(${v_project})
+    add_executable(${v_project_alias} ALIAS ${v_project})
 
-	m_set_target_flags(${v_project})
+    m_set_target_flags(${v_project})
 endmacro()
 
 macro(m_set_target_flags _v_target_name)
 
-	target_compile_options(${_v_target_name} PUBLIC
-			"$<$<C_COMPILER_ID:MSVC>:/utf-8>"
-			"$<$<CXX_COMPILER_ID:MSVC>:/utf-8>"
-	)
+    target_compile_options(${_v_target_name} PUBLIC
+            "$<$<C_COMPILER_ID:MSVC>:/utf-8>"
+            "$<$<CXX_COMPILER_ID:MSVC>:/utf-8>"
+    )
 
-	target_compile_features(${_v_target_name} PUBLIC
-			cxx_std_20
-	)
+    target_compile_features(${_v_target_name} PUBLIC
+            cxx_std_20
+    )
 endmacro()
 
 macro(m_configure_project_env _v_target_name)
-	set(v_project "${v_package_name}_${_v_target_name}")
-	set(v_project_alias "${v_package_namespace}${_v_target_name}")
+    set(v_project "${v_package_name}_${_v_target_name}")
+    set(v_project_alias "${v_package_namespace}${_v_target_name}")
 endmacro()
